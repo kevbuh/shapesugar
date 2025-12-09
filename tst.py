34d68043c56ed19579cfe4338d574d2e2523a2f9
Kevin Buhler <buhlerkw@gmail.com>
2025-12-09 12:00:30 +0000

    min example tst.py

--- tst.py
+++ tst.py
@@ -1,16 +1,21 @@
-from typing import NewType, TypeVarTuple
+from typing import NewType, TypeVarTuple, Self
+
+Shape = TypeVarTuple("Shape")
 
 class Array[*Shape]:
-
-    def __init__(self, shape: tuple[*Shape]):
-        self._shape: tuple[*Shape] = shape
-
-    def get_shape(self) -> tuple[*Shape]:
-        return self._shape
+    def __init__(self, shape: tuple[*Shape]): self._shape: tuple[*Shape] = shape
+    def __abs__(self: Self) -> Self: return self
+    def __add__(self: Self, other: Self) -> Self: return self
 
 Height = NewType('Height', int)
 Width = NewType('Width', int)
+
 shape = (Height(480), Width(640))
 x: Array[Height, Width] = Array(shape)
-y = abs(x)  # Inferred type is Array[Height, Width]
-z = x + x   #        ...    is Array[Height, Width]
+print(x._shape)
+
+y = abs(x)  # Array[Height, Width]
+z = x + x   # Array[Height, Width]
+
+print(y._shape)
+print(z._shape)
